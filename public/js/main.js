afd63efa5b1b6d42a6f91bdf09c31ff06fd9e64d
jordanfranklin <jordanf2010@gmail.com>
2017-03-05 13:10:02 -0600

    CSV can now be read and parsed, not efficiently since I hardcoded. Wish I knew how to concatenate

--- public/js/main.js
+++ public/js/main.js
@@ -61,18 +61,35 @@ document.addEventListener('DOMContentLoaded', function() {
 		//Relevant contestInfo should be saved in a value called contestInfo
 		var csv = new XMLHttpRequest();
 		csv.open('GET', "../js/lib/AllStats.csv", true);
+		var allPlayers = [];
 		csv.onreadystatechange = function(){
 			if (csv.status == 404) {
                 console.log("ERROR");
             }
 			if(csv.readyState == 4 && csv.status == 200) {
-				console.log(csv.statusText);
-				console.log(csv.responseText);
-				var allPlayers = $.csv.toObjects(csv.responseText);
+				//console.log(csv.statusText);
+				//console.log(csv.responseText);
+				//var allPlayers = $.csv.toObjects(csv.responseText);
+				allPlayers = processData(csv.responseText);
+				console.log(allPlayers);
+			}
+			var players = [];
+
+			var user_ids = contestInfo.entries.user_ids;
+
+			for(var i = 0; i<allPlayers.length; i++){
+				for(var j = 0; j < user_ids.length; j++){
+					if("PlayerID:1" === allPlayers[i][1]){
+						players.push(allPlayers[i]);
+						console.log(allPlayers[i]);
+					}
+				}
 			}
 		};
 		csv.send();
 		
+		
+
 		/*reader.onload = function(event) {
 			var csv = event.target.result;
 		
@@ -104,4 +121,24 @@ function readFile() {
 	if(reader.readyState==4) {
 		console.log(reader.responseText);
     }
+}
+
+function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            var tarr = [];
+            for (var j=0; j<headers.length; j++) {
+                tarr.push(headers[j]+":"+data[j]);
+            }
+            lines.push(tarr);
+        }
+    }
+    return lines;
+     //console.log(lines);
 }
