3bd377f03358f74d85ce9a576e9c0cc739631130
Jarett <jarett@wustl.edu>
2017-04-05 15:55:11 -0500

    no longer sending old contests from back end to front end index page

--- controllers/index.js
+++ controllers/index.js
@@ -6,6 +6,13 @@ module.exports = {
 			.lean()
 			.exec(function(err, contests) {
 				if (err) return next(err);
+				for (var i = 0; i < contests.length; i++) {
+					var contestDate = new Date(contests[i].endDate);
+					var todaysDate = new Date();
+					if (contestDate < todaysDate) {
+						contests.splice(i, 1);
+					}
+				}
 				res.render('index', {
 					contests: JSON.stringify(contests)
 				});
